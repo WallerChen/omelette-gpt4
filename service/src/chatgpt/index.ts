cabcf7d9ace9c0cdb6a12a1b07cda4657cafe1ef
ouyangcongyu <weiruiqw@gmail.com>
2023-04-08 10:35:42 +0800

    update model gpt4

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -35,7 +35,7 @@ let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
     const OPENAI_API_MODEL = process.env.OPENAI_API_MODEL
     const model = (typeof OPENAI_API_MODEL === 'string' && OPENAI_API_MODEL.length > 0)
       ? OPENAI_API_MODEL
-      : 'gpt-4-0314'
+      : 'gpt-4'
 
     const options: ChatGPTAPIOptions = {
       apiKey: process.env.OPENAI_API_KEY,
